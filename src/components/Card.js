e450079fa778fd038f97ae47bfaef79dbf1aa5fe
Johnatan <jf.johnatan@gmail.com>
2021-01-21 17:34:50 +0700

    change styling of completed todos, completed todos are moved to the bottom of the list

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,6 @@ import styled from 'styled-components'
 const StyledCard = styled.div`
   box-shadow: 5px 5px 2px rgba(0, 0, 0, 0.5);
   border-radius: 10px;
-  background: white;
   margin: 80px auto 35px auto;
   width: 45%;
   min-width: 350px;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import TodoSettings from './TodoSettings'
-import styled, { keyframes } from 'styled-components'
+import styled, { keyframes, css } from 'styled-components'
 
 const todoColourFade = keyframes`
   from {
@@ -33,15 +33,18 @@ const List = styled.li`
   font-size: 16px;
   padding: 20px;
   border-top: 1px solid black;
+  background: white;
+  opacity: ${props => props.todo.completed ? '70%' : '100%'};
   
   &:hover {
-    animation: ${todoColourFade} 0.1s forwards;
+    animation: ${props => !props.todo.completed ? css`${todoColourFade} 0.1s forwards` : ''}
   }
 `
 
 const Task = styled.span`
   width: 50%;
   word-wrap: break-word;
+  text-decoration: ${props => props.todo.completed ? 'line-through' : ''}
 `
 
 const Delete = styled.i`
@@ -52,13 +55,13 @@ const Delete = styled.i`
 
 const Todo = ({ todo, handleTodoDelete, handlePriorityChange, handleDueDateChange, toggleCompleted }) => {
   return (
-    <List>
+    <List todo={todo}>
       <input 
         type='checkbox' 
         onClick={() => toggleCompleted(todo.id)}
         defaultChecked={ todo.completed ? true : false }
       />
-      <Task>{todo.task}</Task>
+      <Task todo={todo}>{todo.task}</Task>
       <TodoSettings todo={todo} handlePriorityChange={handlePriorityChange} handleDueDateChange={handleDueDateChange} />
       <Delete className="fas fa-trash-alt" onClick={() => handleTodoDelete(todo.id)}>
       </Delete>
@@ -67,8 +70,6 @@ const Todo = ({ todo, handleTodoDelete, handlePriorityChange, handleDueDateChang
 }
 
 const Todos = ({ todos, sorter, handleTodoDelete, handlePriorityChange, handleDueDateChange, toggleCompleted }) => {
-  
-  console.log(todos)
 
   const sortByPriority = (a, b) => {
     const categories = ['high priority', 'medium priority', 'low priority']
@@ -92,11 +93,25 @@ const Todos = ({ todos, sorter, handleTodoDelete, handlePriorityChange, handleDu
       return 0
     }
   }
+
+  const sortByCompleted = (a, b) => {
+    const categories = [true, false]
+    const indexA = categories.indexOf(a.completed)
+    const indexB = categories.indexOf(b.completed)
+    if (indexA > indexB) {
+      return -1
+    } else if (indexA < indexB) {
+      return 1
+    } else {
+      return 0
+    }
+  }
   
   return (
     <ul>
       {todos
         .sort(sorter === 'priority' ? sortByPriority : sorter === 'due date' ? sortByDueDate : undefined)
+        .sort(sortByCompleted)
         .map(todo => (
           <Todo 
             key={todo.id} 
