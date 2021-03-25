0c0f7c34f85f7feafe1bb99aaf19b989296a97f4
Johnatan <jf.johnatan@gmail.com>
2021-03-25 14:53:59 +0700

    change sort menu

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
+import iconSet from "../img/selection.json";
+import IcomoonReact, { iconList } from "icomoon-react";
 
 const StyledFilter = styled.div`
   padding: 10px 25px;
   display: flex;
-  flex-direction: reverse-column;
+  flex-direction: row;
   align-items: center;
   font-family: Open Sans, sans-serif;
   background: #d9d9d9;
 `
 
-const StyledSortMenu = styled.select`
-  position: relative; 
-  left: 10px;
-  font-family: Open Sans, sans-serif;
-  background: transparent;
+const StyledButtonCollection = styled.div`
+  margin-left: 1rem;
+  display: flex;
+  align-items: center;
+`
+
+const StyledButton = styled.button`
+  background-color: transparent;
+  
+  height: 3rem;
+  width: 3rem;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 800;
+  font-size: 1.25rem;
+  margin: 0 .5rem;
+  border-radius: 3px;
   border: 0;
-  font-size: 16px;
-  min-width: 75px;
-  &:focus {
-    outline: none;
+  cursor: pointer;
+  transition: all .1s;
+  
+  background-color: ${props => props.className === 'active' ? '#333' : 'transparent'};
+  color: ${props => props.className === 'active' ? 'white' : '#333'};
+  fill: ${props => props.className === 'active' ? 'white' : '#333'};
+
+  .icon {
+    width: 70%;
+  }
+
+  &:hover {
+    background-color: #333;
+    color: white;
+    fill: white;
   }
 `
 
@@ -28,10 +52,15 @@ const Sorter = ({ sorter, setSorter }) => {
   return (
     <StyledFilter>
       <label>Sort by:</label>
-      <StyledSortMenu defaultValue={sorter} onChange={event => setSorter(event.target.value)}>
-        <option>priority</option>
-        <option>due date</option>
-      </StyledSortMenu>
+
+      <StyledButtonCollection>
+        <StyledButton className={sorter === 'priority' ? 'active' : ''} onClick={() => setSorter('priority')}>
+          !!!
+        </StyledButton>
+        <StyledButton className={sorter === 'due date' ? 'active' : ''} onClick={() => setSorter('due date')}>
+          <IcomoonReact className="icon" iconSet={iconSet} icon="alarm" />
+        </StyledButton>
+      </StyledButtonCollection>
     </StyledFilter>
   )
 }
